10f55837347465906be687f17c40a5de37c750dd
mKopac <121977856+mKopac@users.noreply.github.com>
2025-10-27 08:57:48 +0100

    Fixed header and footer path in login form

--- frontend/odborna-prax-web/src/forms/LoginForm.tsx
+++ frontend/odborna-prax-web/src/forms/LoginForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./../css/LoginForm.css";
 
 type Props = {

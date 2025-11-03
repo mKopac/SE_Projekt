0776fa023d18fc3abecb1f9339d9f1839c0b60ca
mKopac <121977856+mKopac@users.noreply.github.com>
2025-11-03 11:27:07 +0100

    Fixed duplicate method

--- backend/odborna-prax-api/src/main/java/sk/team8/odborna_prax_api/config/SecurityConfig.java
+++ backend/odborna-prax-api/src/main/java/sk/team8/odborna_prax_api/config/SecurityConfig.java
@@ -79,19 +79,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-    @Bean
     public AuthenticationEntryPoint unauthorizedEntryPoint() {
         return (request, response, authException) -> {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());

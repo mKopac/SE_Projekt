e84731ffc756d3f6e127cdea85291c6c5d8abaf8
matos237 <mato.svrcek237@gmail.com>
2025-12-02 17:59:52 +0100

    INTERN-130 Fix header overflow
    Bugfixing for the header being pushed out of view by the page content. Added some wrappers and CSS which fixed this issue on all pages within the app.

--- frontend/odborna-prax-web/src/pages/DashboardPage.tsx
+++ frontend/odborna-prax-web/src/pages/DashboardPage.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Dashboard from '../components/Dashboard';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './../App.css';
 
 const DashboardPage: React.FC = () => {
   return (
-    <div>
-        <Header/>
-      <Dashboard />
-      <Footer/>
+    <div className="app-layout">
+      <Header />
+
+      <main className="main-content">
+  <div className="dashboard-container">
+    <Dashboard />
+  </div>
+</main>
+
+
+      <Footer />
     </div>
   );
 };
